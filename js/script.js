60d33be60cb25e9bd062f0d5ca70db112822b3b9
tpo-aeclarke <aeclarke@travelers.com>
2021-01-12 14:15:22 -0500

    formatting fixes

--- js/script.js
+++ js/script.js
@@ -71,12 +71,12 @@ function printQuote() {
 
   //Check if citation is blank - if not, populate in htmlString
   if (`${quoteOnPage.citation}` !== '') {
-    htmlString += `<span class="citation">``${quoteOnPage.citation}``</span>`
+    htmlString += `<span class="citation">`+`${quoteOnPage.citation}`+`</span>`
     };
 
   //Check if year is blank - if not, populate in htmlString
   if (`${quoteOnPage.year}` !== '') {
-    htmlString += `<span class="year">``${quoteOnPage.year}``</span>`
+    htmlString += `<span class="year">`+`${quoteOnPage.year}`+`</span>`
     };
   
   htmlString += `</p>`
