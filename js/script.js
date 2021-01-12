75930e14e2d4fc5a990afbd3dc6ce4b866dcfd88
tpo-aeclarke <aeclarke@travelers.com>
2021-01-12 15:04:06 -0500

    span class fixes

--- js/script.js
+++ js/script.js
@@ -66,16 +66,19 @@ function printQuote() {
 
   let htmlString =
   `<p class="quote">${quoteOnPage.quote}</p>
-  <p class="source">${quoteOnPage.source}`;
+  <p class="source">${quoteOnPage.source}
+  `;
 
   //Check if citation is blank - if not, populate in htmlString
   if (`${quoteOnPage.citation}` !== '') {
-    htmlString += `<span class="citation">${quoteOnPage.citation}`
+    htmlString += `<span class="citation">${quoteOnPage.citation}</span>
+`
     } else htmlString += ``;
 
   //Check if year is blank - if not, populate in htmlString
   if (`${quoteOnPage.year}` !== '') {
-    htmlString += `<span class="year">${quoteOnPage.year}`
+    htmlString += `<span class="year">${quoteOnPage.year}</span>
+    `
     } else htmlString += ``;
   
   htmlString += `</p>`
