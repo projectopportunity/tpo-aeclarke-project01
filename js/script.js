3fef51a5c372e949539081dd3733039c16d2d3a0
tpo-aeclarke <aeclarke@travelers.com>
2021-01-12 14:58:18 -0500

    span class fixes?

--- js/script.js
+++ js/script.js
@@ -75,7 +75,7 @@ function printQuote() {
 
   //Check if year is blank - if not, populate in htmlString
   if (`${quoteOnPage.year}` !== '') {
-    htmlString += `<span class="year">${quoteOnPage.year}</span>`
+    htmlString += `<span class="year">${quoteOnPage.year}`
     } else htmlString += ``;
   
   htmlString += `</p>`
